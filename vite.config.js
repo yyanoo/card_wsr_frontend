b1f9c6047a4abd1dbc0efee5739185e67283d886
yyanoo <junhong07245@gmail.com>
2025-06-03 02:13:22 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/card_wsr/",
   plugins: [vue()],
-})
+});
