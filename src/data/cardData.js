afa330f9090b179c34d911d9502b0a3c441d30d6
yyanoo <junhong07245@gmail.com>
2025-06-13 07:32:24 +0900

    update

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,15 +1,18 @@
 import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { getCard } from "../service/api";
+import { useSearchStore } from "../stores/searchStore";
 
 export function useCardData() {
   const route = useRoute();
+  const searchStore = useSearchStore();
   const cards = ref([]);
 
   const fetchCards = async (series) => {
     try {
       const res = await getCard(series);
       cards.value = res.data;
+      searchStore.selectedSeries = series;
     } catch (err) {
       console.error("載入卡片資料失敗", err);
       cards.value = [];
