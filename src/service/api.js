b7d0c2a102c1546fe0bf57987aca524546bb3191
yyanoo <junhong07245@gmail.com>
2025-06-12 23:42:58 +0900

    update

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url_card = "https://raw.githubusercontent.com/yyanoo/test/main";
+const url_card = "https://backend-33mc.onrender.com/cards";
 
 export function getCard(id) {
-  return axios.get(`${url_card}/${id}.json`);
+  return axios.get(`${url_card}/${id}`);
 }
